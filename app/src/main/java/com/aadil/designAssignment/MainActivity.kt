4aa7f6c140b321d03a18d8ac9ceabf26f7c548b7
Aadil <aadil.iqubal@proclivistech.com>
2020-11-01 23:58:21 +0530

    Initial commit

---
+++ app/src/main/java/com/aadil/designAssignment/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.aadil.designAssignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+    }
+}
